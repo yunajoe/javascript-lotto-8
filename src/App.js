223a71a1298759f3b994ec15cd4ad383f67cc966
yunajoe <yunajoe@gmail.com>
2025-10-30 21:39:00 +0900

    feat: input클래스 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Input from './input/read-input.js';
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmountInput = new Input('구입금액을 입력해주세요\n');
+    purchaseAmountInput.getInputMessage();
+  }
 }
 
 export default App;
---
+++ src/input/read-input.js
@@ -0,0 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class Input {
+  // 필드
+  // 생성자
+  // 메서드
+
+  constructor(message) {
+    if (!message) {
+      throw new Error('메시지를 입력해주세요');
+    }
+    this.message = message;
+  }
+
+  async getInputMessage() {
+    await Console.readLineAsync(this.message);
+  }
+}
+
+export default Input;
