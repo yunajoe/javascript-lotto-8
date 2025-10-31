2a49eeaf9e70d7b95ff849946d768cf244982e5e
yunajoe <yunajoe@gmail.com>
2025-11-01 08:27:55 +0900

    test: Input클래스 관련 테스팅 로직 추가

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,14 +1,31 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import { INPUT_MESSAGE } from '../src/const/input';
 import Input from '../src/input/read-input';
 // npx jest InputTest
+
 describe('입력 클래스 테스트', () => {
+  const getLogSpy = () => {
+    const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+    logSpy.mockClear();
+    return logSpy;
+  };
+
   test('메세지를 입력하지 않은 경우', () => {
     expect(() => {
       new Input('');
     }).toThrow('[ERROR] 메시지를 입력해주세요');
   });
-  test('메세지를 입력한 경우', () => {
+
+  test('구입 금액 메세지를 입력c한 경우', () => {
     const input = new Input(INPUT_MESSAGE.PURCHASE_AMOUNT);
     expect(input.message).toMatch(/구입금액을 입력해주세요.\n/);
+  });
+  test('당첨 번호  메세지를 입력한 경우', () => {
+    const input = new Input(INPUT_MESSAGE.PURCHASE_AMOUNT);
+    expect(input.message).toMatch(/당첨 번호를 입력해 주세요.\n/);
+  });
+  test('보너스 번호 메세지를 입력한 경우', () => {
+    const input = new Input(INPUT_MESSAGE.PURCHASE_AMOUNT);
+    expect(input.message).toMatch(/보너스 번호를 입력해 주세요.\n/);
   });
 });
--- src/const/input.js
+++ src/const/input.js
@@ -1,5 +1,7 @@
 const INPUT_MESSAGE = {
   PURCHASE_AMOUNT: '구입금액을 입력해주세요.\n',
+  WINNING_NUMBER: '당첨 번호를 입력해 주세요.\n',
+  BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
 };
 
 export { INPUT_MESSAGE };
