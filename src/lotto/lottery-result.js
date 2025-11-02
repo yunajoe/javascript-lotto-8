f674951191b457d00d40d56149b663c130288539
yunajoe <yunajoe@gmail.com>
2025-11-03 00:49:50 +0900

    feat: 수익률 계산 로직 구현

--- src/lotto/lottery-result.js
+++ src/lotto/lottery-result.js
@@ -15,7 +15,13 @@ class LotteryResult {
   }
 
   #calculateLottoWinner() {
-    const initialResult = { 3: 0, 4: 0, 5: 0, '5Bonus': 0, 6: 0 };
+    const initialResult = {
+      3: { count: 0, amount: 5000 },
+      4: { count: 0, amount: 50000 },
+      5: { count: 0, amount: 1500000 },
+      '5Bonus': { count: 0, amount: 30000000 },
+      6: { count: 0, amount: 2000000000 },
+    };
 
     return this.#validLottoTickets.reduce((acc, item) => {
       let cnt = 0;
@@ -26,9 +32,9 @@ class LotteryResult {
       });
 
       if (cnt === 5 && item.includes(this.#bonusNumber)) {
-        acc['5Bonus'] += 1;
+        acc['5Bonus'].count += 1;
       } else if (cnt >= 3) {
-        acc[cnt] += 1;
+        acc[cnt].count += 1;
       }
 
       cnt = 0;
@@ -36,8 +42,20 @@ class LotteryResult {
     }, initialResult);
   }
 
-  printLotteryResult(result) {
-    console.log('result ===>', result);
+  #calculateProfitRate(result) {
+    const pay = this.#validLottoTickets.length * 1000;
+    console.log('pay', pay, result);
+    const profitSum = Object.entries(result).reduce((acc, [key, value]) => {
+      const { count, amount } = value;
+      acc += count * amount;
+      return acc;
+    }, 0);
+    const profitRate = (profitSum / pay) * 100;
+    const rounded = Math.round(profitRate * 10) / 10;
+    return rounded;
+  }
+
+  printLotteryResult(result, result2) {
     Console.print('당첨 통계');
     Console.print('---');
     const PRIZE_RULES = [
@@ -49,14 +67,17 @@ class LotteryResult {
     ];
 
     PRIZE_RULES.forEach(({ key, label }) => {
-      const count = result[key] ?? 0;
+      const count = result[key].count ?? 0;
       Console.print(`${label} - ${count}개`);
     });
+
+    Console.print(`총 수익률은 ${result2}%입니다.`);
   }
 
   #run() {
     const result = this.#calculateLottoWinner();
-    this.printLotteryResult(result);
+    const result2 = this.#calculateProfitRate(result);
+    this.printLotteryResult(result, result2);
   }
 }
 
